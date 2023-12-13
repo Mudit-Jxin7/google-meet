e839a8cd9363adc333a77b285cb5bfaf5470f190
unknown <muditert34@gmail.com>
2023-12-13 22:15:41 +0530

    add basic client and server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {BrowserRouter} from "react-router-dom"
+import "./index.css";
+import {SocketProvider} from "../src/context/SocketProvider.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <React.StrictMode>
+    <SocketProvider>
     <App />
-  </React.StrictMode>,
+    </SocketProvider>
+  </React.StrictMode>
+  </BrowserRouter>
+  ,
 )
